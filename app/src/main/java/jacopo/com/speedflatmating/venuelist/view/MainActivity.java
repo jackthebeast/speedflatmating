f7cd728482fcc75c99c5c0442c4c401ce7c47579
jack_the_beast <jacopo.gra@gmail.com>
2018-09-25 22:42:27 +0100

    added main activity

---
+++ app/src/main/java/jacopo/com/speedflatmating/venuelist/view/MainActivity.java
@@ -0,0 +1,13 @@
+package jacopo.com.speedflatmating.venuelist.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
